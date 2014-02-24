b2954cba32a172c3d8f5c4e1ece34ccaf62b36e2
Matthew Gordon <matthew.gordon2@hp.com>
2014-02-24 14:47:02 +0000

    [idol-admin] (CCUK-1958) Added default host discovery port to settings page and config file. [rev: alex.scown]
    [git-p4: depot-paths = "//depot/products/api/frontend/settings/trunk/": change = 1140075]

---
+++ src/main/exports/settings/js/widgets/port-widget.js
@@ -0,0 +1,39 @@
+define([
+    'settings/js/widget',
+    'text!settings/templates/widgets/port-widget.html'
+], function(Widget, template) {
+
+    return Widget.extend({
+        className: Widget.prototype.className + ' form-horizontal',
+        template: _.template(template),
+
+        render: function() {
+            Widget.prototype.render.call(this);
+            this.$el.append(this.template({strings: this.strings}));
+            this.$port = this.$('input[name="port"]');
+        },
+
+        getConfig: function() {
+            return {
+                port: parseInt(this.$port.val(), 10)
+            };
+        },
+
+        updateConfig: function(config) {
+            Widget.prototype.updateConfig.apply(this, arguments);
+            this.$port.val(config.port);
+        },
+
+        validateInputs: function() {
+            var port = parseInt(this.$port.val(), 10);
+
+            if (isNaN(port) || port <= 0 || port > 65535) {
+                this.updateInputValidation(this.$port);
+                return false;
+            }
+
+            return true;
+        }
+    });
+
+});
---
+++ src/test/js/spec/widgets/port-widget.js
@@ -0,0 +1,87 @@
+define([
+    'settings/js/widgets/port-widget',
+    'test/test-utils'
+], function(PortWidget, utils) {
+
+    describe('Port widget', function() {
+        beforeEach(function() {
+            this.widget = new PortWidget({
+                configItem: 'thingThatNeedsAPort',
+                description: 'Choose a port',
+                strings: {
+                    label: 'Port:'
+                },
+                title: 'Port To Thing'
+            });
+
+            this.widget.render();
+            this.widget.updateConfig({port: 666});
+            this.$port = this.widget.$('input[name="port"]');
+        });
+
+        utils.testSuperProperties.call(this, {configItem: 'thingThatNeedsAPort', title: 'Port To Thing', description: 'Choose a port'});
+
+        it('should display the correct config', function() {
+            expect(this.$port).toHaveValue('666');
+        });
+
+        it('should return the correct config', function() {
+            this.$port.val(42);
+            expect(this.widget.getConfig()).toEqual({port: 42});
+        });
+
+        it('should fail client side validation on blank port', function() {
+            var $controlGroup = this.$port.closest('.control-group');
+            this.$port.val('');
+            var isValid = this.widget.validateInputs();
+
+            expect(isValid).toBeFalsy();
+            expect($controlGroup).toHaveClass('error');
+        });
+
+        it('should fail client side validation on port equal to zero', function() {
+            var $controlGroup = this.$port.closest('.control-group');
+            this.$port.val(0);
+            var isValid = this.widget.validateInputs();
+
+            expect(isValid).toBeFalsy();
+            expect($controlGroup).toHaveClass('error');
+        });
+
+        it('should fail client side validation on port less than zero', function() {
+            var $controlGroup = this.$port.closest('.control-group');
+            this.$port.val(-10);
+            var isValid = this.widget.validateInputs();
+
+            expect(isValid).toBeFalsy();
+            expect($controlGroup).toHaveClass('error');
+        });
+
+        it('should fail client side validation on port greater than 65536', function() {
+            var $controlGroup = this.$port.closest('.control-group');
+            this.$port.val(65536 + 10);
+            var isValid = this.widget.validateInputs();
+
+            expect(isValid).toBeFalsy();
+            expect($controlGroup).toHaveClass('error');
+        });
+
+        it('should pass client side validation on port greater than zero but less than 65536', function() {
+            var $controlGroup = this.$port.closest('.control-group');
+            this.$port.val(101);
+            var isValid = this.widget.validateInputs();
+
+            expect(isValid).toBeTruthy();
+            expect($controlGroup).not.toHaveClass('error');
+        });
+
+        it('should remove validation formatting on input change', function() {
+            var $controlGroup = this.$port.closest('.control-group');
+            this.$port.val('');
+            this.$port.val(101).trigger('change');
+
+            expect($controlGroup).not.toHaveClass('error');
+        });
+    });
+
+});
