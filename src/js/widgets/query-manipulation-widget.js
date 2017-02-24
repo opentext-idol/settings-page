ce368b6f83d979966a9985ed2b6fe0e186856408
Matthew Gordon <matthew.gordon2@hpe.com>
2017-02-24 10:26:32 +0000

    [IDA-649] Added enable view to query manipulation widget.
    This is added in Find and is now required in IDA as well.
    
    [rev: alex.scown]

--- src/js/widgets/query-manipulation-widget.js
+++ src/js/widgets/query-manipulation-widget.js
@@ -10,11 +10,20 @@ define([
 ], function(AciWidget, EnableView, queryManipulationTemplate) {
 
     return AciWidget.extend({
-
+        EnableView: EnableView,
         queryManipulationTemplate: _.template(queryManipulationTemplate),
 
         initialize: function() {
             AciWidget.prototype.initialize.apply(this, arguments);
+
+            this.enableView = new this.EnableView({
+                enableIcon: 'fa fa-file',
+                strings: this.strings
+            });
+
+            this.listenTo(this.enableView, 'change', function() {
+                this.$('.settings-required-flag').toggleClass('hide', !this.enableView.getConfig());
+            });
         },
 
         render: function() {
@@ -27,19 +36,32 @@ define([
             }));
 
             this.$typeahead = this.$('.typeahead-input');
+
+            this.enableView.render();
+            $validateButtonParent.before(this.enableView.el);
         },
 
         getConfig: function() {
             return {
+                enabled: this.enableView.getConfig(),
                 server: AciWidget.prototype.getConfig.call(this),
                 typeAheadMode: this.$typeahead.val()
-            }
+            };
         },
 
         updateConfig: function(config) {
             AciWidget.prototype.updateConfig.call(this, config.server);
 
             this.$typeahead.val(config.typeAheadMode);
+            this.enableView.updateConfig(config.enabled);
+        },
+
+        validateInputs: function() {
+            return !this.enableView.getConfig() || AciWidget.prototype.validateInputs.call(this);
+        },
+
+        shouldValidate: function() {
+            return this.enableView.getConfig();
         }
     });
 
