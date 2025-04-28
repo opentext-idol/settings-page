5fde98a98f9dd8c9dcf7f544bf01a30c64828e3a
Joseph Lansdowne <jlansdowne@opentext.com>
2025-04-28 15:27:13 +0100

    [D573040] insecure JDBC configuration
    don't construct JDBC URL on frontend, leave it to the backend

--- src/js/widgets/paths-widget.js
+++ src/js/widgets/paths-widget.js
@@ -74,8 +74,10 @@ define([
 
             this.listView = new ListView({
                 collection: this.collection,
-                itemTemplate: this.itemTemplate,
-                itemTemplateOptions: {strings: this.strings}
+                itemOptions: {
+                    template: this.itemTemplate,
+                    templateOptions: {strings: this.strings}
+                }
             });
 
             this.listenTo(this.collection, 'add remove reset', this.updateRemoveButtons);
